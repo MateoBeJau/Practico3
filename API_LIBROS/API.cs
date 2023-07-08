2afa305f2bcaeabbf74fe98d6b9f064dc9f7e2d5
Mateo <mateobejau@gmail.com>
2023-07-08 14:56:28 -0300

    Practico 3

---
+++ API_LIBROS/API.cs
@@ -0,0 +1,21 @@
+﻿namespace API_LIBROS
+{
+    public class API
+    {
+        public string getLibros(string texto, string key, string pagina)
+        {
+            UriBuilder builder = new UriBuilder("https://www.googleapis.com/books/v1/volumes");
+            builder.Query = $"?key{key}&startIndex={pagina}&q={texto}";
+
+            //Create a query 
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync(builder.Uri).Result;
+
+            using (StreamReader sr = new StreamReader(result.Content.ReadAsStreamAsync().Result))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+       
+    }
+}
---
+++ WEB_LIBROS/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WEB_LIBROS.Models;
+using API_LIBROS;
+using Newtonsoft.Json;
+
+namespace WEB_LIBROS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {    
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string autor, string titulo, string descripcion, string pagina)
+        {
+            string auxAutor = "";
+            string auxTitulo = "";
+            string auxDescripcion = "";
+
+            if (autor != null)
+            {
+                auxAutor = autor.ToLower();
+                auxAutor = "+inauthor:" + auxAutor;
+            }
+            else if (autor == null) { auxAutor = ""; }
+
+            if (titulo != null)
+            {
+                auxTitulo = titulo.ToLower();
+                auxTitulo = "+intitle:" + auxTitulo;
+            }
+            else if (titulo == null) { auxTitulo = ""; }
+
+            if (descripcion != null)
+            {
+                auxDescripcion = descripcion.ToLower();
+            }
+            else if (descripcion == null) { auxDescripcion = ""; }
+
+            string q = auxDescripcion + auxAutor + auxTitulo;
+
+            API apiBooks = new API();
+
+
+            string resultado = apiBooks.getLibros(q, "AIzaSyAvKWpjkXwzkOCvPCqFMjQMRJkEhY3KKZI", pagina);
+
+            var listaLibros = JsonConvert.DeserializeObject<LIBROS>(resultado);
+
+            if (listaLibros is not null)
+            {
+                ViewBag.Libros = listaLibros.Items;
+            }
+
+            ViewBag.Autor = autor;
+            ViewBag.Titulo = titulo;
+            ViewBag.Desc = descripcion;
+
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
